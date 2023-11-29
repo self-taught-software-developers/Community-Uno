91eef58e319e66018aa9d74277bc971a7841fc0c
Revok <cerve.v55@gmail.com>
2023-11-29 09:09:51 -0500

    created ui package and moved shared ui Main.kt to the ui package.

--- webApp/src/jsMain/kotlin/Main.kt
+++ webApp/src/jsMain/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import org.jetbrains.skiko.wasm.onWasmReady
+import ui.MainScreen
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
