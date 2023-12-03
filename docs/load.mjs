029f3c05acb080fcda112852d4287e0c43f7b3f2
Revok <cerve.v55@gmail.com>
2023-12-02 19:31:34 -0500

    Github pages deployment error resolution.

---
+++ docs/load.mjs
@@ -0,0 +1,12 @@
+// import { instantiate } from './webApp.uninstantiated.mjs';
+//
+// await wasmSetup;
+//
+// let te = null;
+//
+// try {
+//     await instantiate({ skia: Module['asm'] });
+// } catch (e) {
+//   te = e;
+// }
+//
