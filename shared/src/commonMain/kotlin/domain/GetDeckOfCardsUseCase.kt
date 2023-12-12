5ea11fe6640de77a8851277af417b6fac4c9008c
Revok <cerve.v55@gmail.com>
2023-12-12 17:29:23 -0500

    GetNewGameUseCase.kt deletes all cards within the deck then adds new ones.

--- shared/src/commonMain/kotlin/domain/GetDeckOfCardsUseCase.kt
+++ shared/src/commonMain/kotlin/domain/GetDeckOfCardsUseCase.kt
@@ -3,7 +3,9 @@ package domain
 import db.model.Collection
 import db.model.Document
 import dev.gitlive.firebase.firestore.FirebaseFirestore
+import helpers.observe
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapNotNull
 import model.Card
 import org.koin.core.component.KoinComponent
@@ -14,10 +16,5 @@ class GetDeckOfCardsUseCase(
     operator fun invoke(): Flow<List<Card>> = fireStore
         .collection(Collection.GameSession.name)
         .document(Document.GameDeck.name)
-        .snapshots.mapNotNull { snapshot ->
-            try {
-                snapshot.data<Map<String, List<Card>>>()
-                    .values.firstOrNull()
-            } catch (e: Exception) { null }
-        }
+        .observe<List<Card>> { emptyList() }
 }
