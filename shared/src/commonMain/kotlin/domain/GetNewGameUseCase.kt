4546c7fa311081537ed3269db7d4f9be8ca15a52
Revok <cerve.v55@gmail.com>
2023-12-12 17:29:40 -0500

    GetNewGameUseCase.kt deletes all cards within the deck then adds new ones.

--- shared/src/commonMain/kotlin/domain/GetNewGameUseCase.kt
+++ shared/src/commonMain/kotlin/domain/GetNewGameUseCase.kt
@@ -2,7 +2,6 @@ package domain
 
 import db.model.Collection
 import db.model.Document
-import db.model.Field
 import dev.gitlive.firebase.firestore.FirebaseFirestore
 import model.Card
 import org.koin.core.component.KoinComponent
@@ -11,11 +10,23 @@ class GetNewGameUseCase(
     private val fireStore: FirebaseFirestore,
     private val getDeck: GetDeckUseCase
 ) : KoinComponent {
-    suspend operator fun invoke(deck: List<Card> = getDeck.invoke()) {
-        fireStore
+    suspend operator fun invoke(
+        deck: List<Card> = getDeck.invoke()
+    ) {
+        val batch = fireStore.batch()
+        val reference = fireStore
             .collection(Collection.GameSession.name)
             .document(Document.GameDeck.name)
-            .set(hashMapOf(Field.Deck.name to deck))
+
+        batch.delete(reference)
+
+        deck.forEach { card ->
+            batch.set(
+                documentRef = reference,
+                data = hashMapOf(card.uuid to card),
+                merge = true
+            )
+        }.also { batch.commit() }
     }
 
 }
