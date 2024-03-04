98a0e3a575d68051c0871c4ac116908341c42380
Revok <cerve.v55@gmail.com>
2024-03-04 15:17:09 -0500

    2024 revision in preparation for project showcase on 3/13/2024. GameSession.kt added. This class provides a high level view of the game.

---
+++ shared/src/commonMain/kotlin/newmodel/GameSession.kt
@@ -0,0 +1,15 @@
+package newmodel
+
+sealed interface GameSession {
+
+    data class Entity(
+        val adminId: String?,
+        val currentDealerId: String?,
+        val currentPlayerId: String?,
+        val playerList: List<Player.Entity>,
+        val cardList: List<Card.Entity>,
+        val gameBoardDirection: GameBoardDirection,
+        val gameState: GameState
+    )
+
+}
