12fd7835e96dc704d8f520edc0eac60e2d5d9840
Revok <cerve.v55@gmail.com>
2024-03-04 15:58:14 -0500

    2024 revision in preparation for project showcase on 3/13/2024. Card.kt added.

---
+++ shared/src/commonMain/kotlin/newmodel/Card.kt
@@ -0,0 +1,43 @@
+package newmodel
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Block
+import androidx.compose.material.icons.filled.Loop
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed interface Card {
+    data class Entity(
+        val id: String,
+        val ownerId: String,
+        val properties: Properties,
+        val isPlayed: Boolean
+    ) : Card
+
+    data class Properties(
+        val type: CardType,
+        val color: CardColor
+    )
+
+    enum class CardColor(val value: Color) {
+        RED(Color.Red),
+        BLUE(Color.Blue),
+        YELLOW(Color.Yellow),
+        GREEN(Color.Green),
+        BLACK(Color.Black)
+    }
+
+    enum class CardType(var value: CardValue) {
+        NUMERIC(CardValue.Text("1..9")),
+        DRAW_2(CardValue.Text("+2")),
+        DRAW_4(CardValue.Text("+4")),
+        WILD(CardValue.Text("Wild")),
+        SKIP(CardValue.Icon(Icons.Default.Block)),
+        REVERSE(CardValue.Icon(Icons.Default.Loop))
+    }
+
+    sealed interface CardValue {
+        data class Text(val value: String) : CardValue
+        data class Icon(val value: ImageVector) : CardValue
+    }
+}
