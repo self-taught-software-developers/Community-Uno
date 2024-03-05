13b05954bbf9ba7f168c04a38930d3ed1935f9fe
Revok <cerve.v55@gmail.com>
2024-03-05 15:25:41 -0500

    2024 revision in preparation for project showcase on 3/13/2024. Implement player hand visually.

--- shared/src/commonMain/kotlin/ui/GameTableLayout.kt
+++ shared/src/commonMain/kotlin/ui/GameTableLayout.kt
@@ -4,18 +4,17 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import newmodel.Card
 import newmodel.Player
@@ -42,34 +41,26 @@ fun GameTableLayout(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        BoxWithConstraints(
+
+        StarLayout(
             modifier = Modifier.weight(3f),
-            contentAlignment = Alignment.Center
+            radius = 300.dp
         ) {
-
-            StarLayout(
-                radius = maxWidth / 4F
-            ) {
-                otherPlayerList.forEachIndexed { index, player ->
-                    PlayerHandLayout(
-                        player = player
-                    )
-                }
-            }
-
+//            otherPlayerList.drop(1).forEachIndexed { index, player ->
+//                PlayerHandLayout(
+//                    player = player
+//                )
+//            }
         }
 
         if (activePlayer != null) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                PlayerHandLayout(
-                    player = activePlayer,
-                    isActivePlayer = true
-                )
-            }
+
+            PlayerHandLayout(
+                modifier = Modifier.weight(1f),
+                player = activePlayer,
+                isActivePlayer = true
+            )
+
         }
     }
 
@@ -79,12 +70,14 @@ fun GameTableLayout(
 fun PlayerHandLayout(
     player: Player.UI,
     modifier: Modifier = Modifier,
-    angle: Double = 45.0,
+    angle: Double = 25.0,
     isActivePlayer: Boolean = false,
-    spacing: Int = if (isActivePlayer) 60 else 10
 ) {
 
-    Box(modifier = modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
         player.hand.forEachIndexed { index, card ->
             val interactionSource = remember { MutableInteractionSource() }
             val isFocused by interactionSource.collectIsHoveredAsState()
@@ -102,12 +95,10 @@ fun PlayerHandLayout(
             PlayingCard(
                 modifier = Modifier
                     .graphicsLayer {
-                        transformOrigin = TransformOrigin.Center
-                        translationX = (index * spacing).toFloat()
+                        transformOrigin = if (isActivePlayer) {
+                            TransformOrigin(0f, (angle/5).toFloat())
+                        } else TransformOrigin(0f, 1f)
                         rotationZ = cardAngle
-    //                    rotationX = -cardAngle
-                        rotationY = -cardAngle
-
                     }.scale(scale).zIndex(zIndex),
                 interactionSource = interactionSource,
                 enabled = isActivePlayer,
@@ -130,10 +121,11 @@ fun StarLayout(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
-    var starRadiusPx = with(LocalDensity.current) { radius.roundToPx() }
-    var totalRadius = 0
-    var maxChildDiameter = 0
-    var count = 0
+    val starRadiusPx = with(LocalDensity.current) { radius.roundToPx() }
+
+    var totalRadius by remember { mutableIntStateOf(0) }
+    var maxChildDiameter by remember { mutableIntStateOf(0) }
+    var count by remember { mutableIntStateOf(0) }
 
     Layout(
         content = content,
